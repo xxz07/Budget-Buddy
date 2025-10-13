3640e756d48c257b82f87167788fa75a1673e9a0
TennisZoom <302056276@student.rocmondriaan.nl>
2025-10-13 09:50:53 +0200

    update on crud check description
    -added a dashboard route
    -added a file called dashboard
    -added a new form for transactions
    -tried adding chartJS for the chart
    -changed the routes in the files for it too all link up correctly
    -for the crud i added a form and a entitymanager
    {{This is still all a prototype until we get a one to many and a many to one relation in our database}}

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Transactions;
+use App\Form\TransactionsType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 final class GeneralController extends AbstractController
 {
@@ -20,6 +25,20 @@ final class GeneralController extends AbstractController
     {
         return $this->render('general/about-us.html.twig', [
             'controller_name' => 'GeneralController',
+        ]);
+    }
+    #[Route('/dashboard', name: 'app_about_us')]
+    public function dashboard(EntityManagerInterface $entityManager): Response
+    {
+        $showTransactions = $entityManager->getRepository(Transactions::class)->findAll();
+
+        $transactions = new Transactions();
+        $form = $this->createForm(TransactionsType::class, $transactions);
+
+        return $this->render('general/dashboard.html.twig', [
+            'controller_name' => 'GeneralController',
+            'form' => $form,
+            'showTransactions' => $showTransactions,
         ]);
     }
 }
